aaab2f375ffe2749d43bc9ff5c13961ed1ab4901
Brett Weese <brett.weese@gmail.com>
2014-10-09 06:31:52 -0500

    initial commit

---
+++ pdb_ex.py
@@ -0,0 +1,19 @@
+import sys
+from random import choice
+
+random1 = [1,2,3,4,5,6,7,8,9,10,11,12]
+random2 = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+while True:
+	print "To Exit this game type 'exit'"
+	answer = raw_input("What is {} times {} ?".format(choice(random2), choice(random1)))
+
+	# exit
+	if answer == "exit":
+		print "Now exiting game!"
+		sys.exit()
+
+	elif answer == choice(random2) * choice(random1):
+		print "Correct!"
+	else:
+		print "Wrong!"
